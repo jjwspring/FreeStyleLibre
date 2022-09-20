564caa615da6033f9d8df488eb42b1c796408133
Joel Williams <joel.williams@springboard.pro>
2022-09-20 17:12:03 +0100

    Add labels to predictor plot

--- blood_glucose_diary.py
+++ blood_glucose_diary.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from numba import jit
 from matplotlib import pyplot as plt
 
 
@@ -26,10 +27,12 @@ class BloodGlucoseDiary:
 
         table = pd.read_csv(filepath, skiprows=1, index_col=2)
         table.index = pd.to_datetime(table.index, format=r'%d-%m-%Y %H:%M')
+        table.index = table.index.tz_localize(self.end_time.tz)
 
         historic_bg = table.loc[table['Record Type'] == 0, r'Historic Glucose mmol/L']
         scan_bg = table.loc[table['Record Type'] == 1, 'Scan Glucose mmol/L']
         self.BG = pd.concat((historic_bg, scan_bg))
+        self.BG = self.BG.sort_index()
 
         self.QA = table.loc[pd.notna(table['Rapid-Acting Insulin (units)']), r'Rapid-Acting Insulin (units)']
         QA_non_numeric = table['Non-numeric Rapid-Acting Insulin'].dropna()
@@ -89,15 +92,136 @@ class BloodGlucoseDiary:
 
         return fig, glucose_ax, insulin_ax, carbs_ax
 
-# todo  Write function for insulin profiles
-# todo  Write function to predict insulin activity
+
+class BloodGlucosePredictor:
+
+    def __init__(self, blood_glucose_diary):
+        self.bgd = blood_glucose_diary
+        self.rapid_on_board = None
+        self.rapid_activity = None
+        self.glucose_rate = self.differentiate(self.bgd.BG)
+        self.carb_activity = None
+
+    @staticmethod
+    def differentiate(time_series, ts=15):
+        resample = time_series.resample(f'{ts}Min')
+        time_series = resample.mean()
+        time_series.interpolate('index')
+        return pd.Series(np.gradient(time_series) / ts, time_series.index)
+
+    @staticmethod
+    def resample_sum(variable, sample_rate=1):
+        resample = variable.resample(f'{sample_rate}Min')
+        return resample.sum()
+
+    def get_rapid_injections(self, sample_rate=1):
+        start = pd.Series([0], [self.bgd.start_time])
+        end = pd.Series([0], [self.bgd.end_time])
+        QA = pd.concat([start, self.bgd.QA, end])
+        return self.resample_sum(QA, sample_rate)
+
+    @staticmethod
+    @jit(nopython=True)
+    def insulin_integration(injections, on_board, concentration, activity, t_steps, a, b, c, d, f, g):
+        for i, step_size in enumerate(t_steps):
+            on_board[i+1] = on_board[i] + step_size * (-f * on_board[i] + g * injections[i])
+            concentration[i+1] = concentration[i] + step_size * (-c * concentration[i] + d * on_board[i])
+            activity[i+1] = activity[i] + step_size * (-a * activity[i] + b * concentration[i])
+
+        return on_board, concentration, activity
+
+    def predict_insulin(self, sensitivity, peak_time, unit_conversion=60*0.0005555555):
+        a = self._clearance_rate_from_peak(peak_time)
+        b = a * sensitivity
+        c = d = f = a
+        g = unit_conversion
+
+        injections = self.get_rapid_injections()
+        inj = np.array(injections)
+        on_board = np.zeros_like(inj)
+        concentration = np.zeros_like(inj)
+        activity = np.zeros_like(inj)
+
+        t_steps = (injections.index[1:] - injections.index[:-1]) / pd.to_timedelta(1, unit='Min')
+        t_s = np.array(t_steps)
+
+        on_board, concentration, activity = self.insulin_integration(inj, on_board, concentration,
+                                                                     activity, t_s, a, b, c, d, f, g)
+
+        self.rapid_on_board = pd.Series(on_board, injections.index)
+        self.rapid_activity = pd.Series(activity, injections.index)
+
+    def get_carbs(self, sample_rate=1):
+        start = pd.Series([0], [self.bgd.start_time])
+        end = pd.Series([0], [self.bgd.end_time])
+        carbs = pd.concat([start, self.bgd.carbs, end])
+        return self.resample_sum(carbs, sample_rate)
+
+    def predict_carbs(self, BG_p_10g, clearance_time=3*60, delay=10):
+        clearance_time = round(clearance_time)
+        carbs = self.get_carbs()
+        unit_activity = np.linspace(1, 0, clearance_time - delay)
+        unit_activity = unit_activity / sum(unit_activity)
+        carb_action_profile = np.concatenate([np.zeros(delay), (BG_p_10g/10) * unit_activity])
+        self.carb_activity = pd.Series(np.convolve(carbs, carb_action_profile)[:len(carbs)], carbs.index)
+        print()
+
+    @staticmethod
+    def _clearance_rate_from_peak(peak_time):
+        return 2 / peak_time
+
+    def plot(self, start_time=None, end_time=None):
+        fig, glucose_ax, insulin_ax, carbs_ax = self.bgd.plot(start_time, end_time)
+
+        rate_ax = glucose_ax.twinx()
+        rate_ax.spines.right.set_position(('axes', 1.4))
+
+        if self.glucose_rate is not None:
+            rate_ax.plot(self.glucose_rate, label='Glucose Rate')
+
+        if self.rapid_activity is not None:
+            rate_ax.plot(self.rapid_activity, label='Rapid Activity')
+
+        if self.carb_activity is not None:
+            rate_ax.plot(self.carb_activity, label='Carb Release')
+
+        net_activity = self.carb_activity + self.rapid_activity
+        rate_ax.plot(net_activity, label='Predicted Rate')
+
+        predicted_glucose = net_activity.cumsum()
+        predicted_start_value = predicted_glucose[predicted_glucose.index.indexer_at_time('03:00:00')]
+
+        daily_offset = predicted_start_value.reindex(predicted_glucose.index)
+        daily_offset.iloc[0] = predicted_glucose.iloc[0]
+        daily_offset = daily_offset.ffill()
+        predicted_glucose = predicted_glucose + daily_offset
+        glucose_ax.plot(predicted_glucose)
+        plt.legend()
+        plt.tight_layout()
+
 # todo  Same for carbohydrate
 # todo  Figure out resampling
 # todo  Get change in blood glucose
 # todo  New plot function to compare insulin activity, carb release and change in blood glucose
 
+
+# input is a set of injections
+# update on_board with
+# https://github.com/LoopKit/Loop/issues/388
+
+# action(k+1) = (k_now - k_previous) * (-a*action(k) + b*concentration(k)) + action(k)
+# concentration(k+1) = (k_now - k_previous) * (-c*concentration(k) + d*on_board(k)) + concentration(k)
+# on_board(k+1) = (k_now - k_previous) * (-f*on_board(k) + g*injection(k)) + on_board(k)
+
 if __name__ == '__main__':
     my_bgdiary = BloodGlucoseDiary()
-    my_bgdiary.load_from_csv('test_data.csv')
-    my_bgdiary.plot(start_time=pd.to_datetime('2022-01-29'), end_time=pd.to_datetime('2022-01-31'))
+    my_bgdiary.load_from_csv('JoelWilliams_glucose_20-9-2022.csv')
+    fig = my_bgdiary.plot(start_time=pd.to_datetime('2022-01-29'), end_time=pd.to_datetime('2022-01-31'))[0]
+
+    bgp = BloodGlucosePredictor(my_bgdiary)
+    bgp.predict_carbs(3)
+    bgp.predict_insulin(-3, 60)
+    bgp.plot(start_time=pd.to_datetime('2022-01-29'), end_time=pd.to_datetime('2022-01-31'))
     plt.show()
+
+
--- test_blood_glucose_diary.py
+++ test_blood_glucose_diary.py
@@ -1,6 +1,43 @@
 import unittest
 import pandas as pd
-from blood_glucose_diary import BloodGlucoseDiary
+import numpy as np
+import timeit
+from blood_glucose_diary import BloodGlucoseDiary, BloodGlucosePredictor
+from matplotlib import pyplot as plt
+
+from scipy import integrate
+import pandas as pd
+import numpy as np
+
+def integrate_method(self, how='trapz', unit='s'):
+    '''Numerically integrate the time series.
+
+    @param how: the method to use (trapz by default)
+    @return
+
+    Available methods:
+     * trapz - trapezoidal
+     * cumtrapz - cumulative trapezoidal
+     * simps - Simpson's rule
+     * romb - Romberger's rule
+
+    See http://docs.scipy.org/doc/scipy/reference/integrate.html for the method details.
+    or the source code
+    https://github.com/scipy/scipy/blob/master/scipy/integrate/quadrature.py
+    '''
+    available_rules = set(['trapz', 'cumtrapz', 'simps', 'romb'])
+    if how in available_rules:
+        rule = integrate.__getattribute__(how)
+    else:
+        print('Unsupported integration rule: %s' % (how))
+        print('Expecting one of these sample-based integration rules: %s' % (str(list(available_rules))))
+        raise AttributeError
+
+    result = rule(self.values, self.index.astype(np.int64) / 10 ** 9)
+    # result = rule(self.values)
+    return result
+
+pd.Series.integrate = integrate_method
 
 
 class TestBloodGlucoseDiary(unittest.TestCase):
@@ -9,7 +46,7 @@ class TestBloodGlucoseDiary(unittest.TestCase):
         bgd = BloodGlucoseDiary()
         bgd.load_from_csv('test_data.csv')
 
-        self.assertEqual(bgd.end_time, pd.to_datetime('2022-2-12 16:39 UTC'))
+        self.assertEqual(bgd.end_time, pd.to_datetime('2022-2-01 17:23 UTC'))
         self.assertEqual(bgd.BG[pd.to_datetime('2021-07-06 23:19')], 4.0)
         self.assertTrue(pd.api.types.is_float_dtype(bgd.BG))
         self.assertEqual(len(bgd.BG), 10)
@@ -31,6 +68,62 @@ class TestBloodGlucoseDiary(unittest.TestCase):
 
         self.assertEqual(bgd.notes[pd.to_datetime('2022-01-28 21:33')], 'Exercise')
         self.assertEqual(len(bgd.notes), 2)
+
+
+class TestBloodGlucosePredictor(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.bgd = BloodGlucoseDiary()
+        self.bgd.load_from_csv('test_data.csv')
+        self.bgp = BloodGlucosePredictor(self.bgd)
+
+    def test_get_rapid_injections(self):
+        insulin = pd.Series([1, 2, np.nan, 4],
+                            pd.to_datetime(['2022/01/01 00:01', '2022/01/01 00:01',
+                                            '2022/01/01 00:02', '2022/01/01 00:04']))
+        self.bgd.QA = insulin
+        self.bgd.start_time = pd.to_datetime('2022/01/01 00:00')
+        self.bgd.end_time = pd.to_datetime('2022/01/01 00:05')
+
+        injections = self.bgp.get_rapid_injections()
+        self.assertEqual(injections['2022/01/01 00:00'], 0)
+        self.assertEqual(injections['2022/01/01 00:01'], 3)
+        self.assertEqual(injections['2022/01/01 00:02'], 0)
+        self.assertEqual(injections['2022/01/01 00:03'], 0)
+        self.assertEqual(injections['2022/01/01 00:04'], 4)
+        self.assertEqual(injections['2022/01/01 00:05'], 0)
+
+    def test_predict_insulin(self):
+        self.bgd = BloodGlucoseDiary()
+        self.bgd.load_from_csv('test_insulin_data.csv')
+        self.bgp = BloodGlucosePredictor(self.bgd)
+        self.bgp.predict_insulin(1, 1*60)
+
+        self.assertEqual(self.bgp.rapid_activity.idxmax(), pd.to_datetime('2021-07-06 01:00:00 UTC'))
+        integral = self.bgp.rapid_activity.integrate('trapz', unit='s') / 60
+        self.assertAlmostEqual(integral, 1, 4)
+        self.bgp.predict_insulin(2, 1*60)
+        self.assertEqual(self.bgp.rapid_activity.idxmax(), pd.to_datetime('2021-07-06 01:00:00 UTC'))
+        integral = self.bgp.rapid_activity.integrate('trapz', unit='s') / 60
+        self.assertAlmostEqual(integral, 2, 4)
+        integral = integrate.cumtrapz(self.bgp.rapid_activity, self.bgp.rapid_activity.index) / pd.to_timedelta('1Min')
+        integral = pd.Series(np.concatenate([[0], integral]), self.bgp.rapid_activity.index)
+        plt.figure(figsize=[10, 6])
+        plt.plot(self.bgp.rapid_on_board, label='Insulin on board')
+        plt.plot(self.bgp.rapid_activity, label='Insulin activity')
+        plt.legend(loc='lower left', bbox_to_anchor=[0, 1.01])
+        ax = plt.twinx(plt.gca())
+        ax.plot((integral[-1]-integral), label='Effect on BG', color='g')
+        plt.legend(loc='lower right', bbox_to_anchor=[1, 1.01])
+        plt.tight_layout()
+        plt.show()
+
+    def test_differentiate(self):
+        time_series = pd.Series([1, 2, 4, 7, 11], index=pd.date_range('2022-01-01 00:00:00 UTC', periods=5, freq='5Min'))
+        bgd = BloodGlucoseDiary()
+        bgd.BG = time_series
+        bgp = BloodGlucosePredictor(bgd)
+        bgp.differentiate(time_series)
 
 
 if __name__ == '__main__':
